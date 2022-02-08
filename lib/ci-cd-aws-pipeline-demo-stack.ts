58ce60916ec8e07cc3d5aaad3822f4a5a553c314
Mesut Doganguzel <mesutguzel3501@gmail.com>
2022-02-08 02:56:30 -0500

    up-to-date

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -1,16 +1,32 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+// import { MyPipelineAppStage } from './stage';
 
-export class CiCdAwsPipelineDemoStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CiCdAwsPipelineDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+     new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('mesutguzel/ci-cd-aws-pipeline-demo', 'main'), //Remember to change 
+        commands: ['npm ci', 
+                   'npm run build', 
+                   'npx cdk synth']
+      }),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CiCdAwsPipelineDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    // const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+    //   env: { account: "755314965794", region: "us-east-1" }
+    // }));
+
+    // testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    // const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+    //   env: { account: "755314965794", region: "us-east-1" }
+    // }));
   }
 }
